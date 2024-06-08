385fb0c1f23394008418b927d4d1c745c4d9c6d5
Tomasz GÓRSKI <tomasz.gorsk88@gmail.com>
2024-06-08 13:20:18 +0200

    Mockito first tests

--- Mockito/src/test/java/goral/SmartHomeControllerTest.java
+++ Mockito/src/test/java/goral/SmartHomeControllerTest.java
@@ -1,31 +1,50 @@
 package goral;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class SmartHomeControllerTest {
+
+    @Mock  HumiditySensor humiditySensor;
+    @Mock  WateringController wateringController;
+
+    private SmartHomeController smartHomeController;
+
+    @BeforeEach
+    public void init(){
+        MockitoAnnotations.openMocks(this);
+        smartHomeController = new SmartHomeController(humiditySensor, wateringController);
+    }
 
     @Test
     public void shouldDisableWatering(){
 
         //given
-        HumiditySensor humiditySensor = Mockito.mock(HumiditySensor.class);
-
-        WateringController wateringController = Mockito.mock(WateringController.class);
-
         Mockito.when(humiditySensor.getHumidity()).thenReturn(50);
-
-        SmartHomeController smartHomeController = new SmartHomeController(humiditySensor, wateringController);
 
         //when
         smartHomeController.enableWateringIfNeeded();
 
         // then
-        Mockito.verify(wateringController).disableWatering();
-
+        verify(wateringController).disableWatering();
     }
 
+    @Test
+    public void shouldEnableWatering(){
 
+        //given
+        Mockito.when(humiditySensor.getHumidity()).thenReturn(49);
+
+        //when
+        smartHomeController.enableWateringIfNeeded();
+
+        // then
+        verify(wateringController).enableWatering();
+    }
 }
